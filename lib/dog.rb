48b74c3db6ff19fd5c3baa36cb94a166863f0ded
Allison Ricker <alliricker@gmail.com>
2020-10-03 19:57:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,12 @@
 # Your code goes here!
 class Dog
-  Dog.name = name
-  Dog.name
-  Dog.bark puts "woof!"
+  def name= ("dogs_name")
+    @this_dogs_name = dogs_name
+  end
+  def name
+    @this_dogs_name
+  end
+  def bark
+    puts "woof!"
+  end
 end
